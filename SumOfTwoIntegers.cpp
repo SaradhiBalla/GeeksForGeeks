91d69e47ea30ee9bd91eadc90ec9adf2a12f3112
Saradhi Balla <saradhi0213@gmail.com>
2024-01-22 22:00:11 +0530

    first commit

---
+++ SumOfTwoIntegers.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main(){
+    int A,B,C; //Declearing integer datatype A,B,C
+    scanf("%d %d",&A,&B);
+    C=A+B; // Sum of A and B is stored in c and displayed
+    printf("%d",C);
+}
+
