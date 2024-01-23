8df44aa2ae94997185a36fe93cb673f0531961f0
Saradhi Balla <saradhi0213@gmail.com>
2024-01-23 18:38:30 +0530

    Using Modulus Operator

---
+++ Using_Modulus_Operator.cpp
@@ -0,0 +1,9 @@
+//Given two positive integers a and b, the task is to find the remainder when a is divided by b.
+#include<stdio.h>
+main(){
+	int a,b,c; //Declaring three integer datatypes as a,b,c
+	scanf("%d %d",&a,&b); //Taking the input from the user
+	c=a%b; //Performing the modulus operation to the variable a and b and assigned to the variable c
+	printf("%d",c);
+	
+}
